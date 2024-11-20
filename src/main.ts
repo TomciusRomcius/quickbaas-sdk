20dd06865367534209a5a61dbb7a624fca86742e
Tomas <tomas.ew88@gmail.com>
2024-11-20 12:04:17 +0200

    Adding signInWIthPassword

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,19 @@ import 'dotenv/config';
 const BACKEND_URL = process.env.BACKEND_URL!;
 
 export class Auth {
-  public static signInWithPassword(email: string, password: string) {
-    
+  public static async signInWithPassword(email: string, password: string) {
+    const response = await fetch(`BACKEND_URL/users`, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    });
+
+    if (response.status === 200) {
+      const jwt = await response.json(); 
+      localStorage.setItem('auth-cookie', jwt);
+      console.log(jwt);
+    }
   }
 }
