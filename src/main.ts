112e2d3f43bab0bf09c2047cbca598f5de34b4fa
Tomas <tomas.ew88@gmail.com>
2024-11-20 11:58:30 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+
+// TODO: Check if the backend url is defined
+const BACKEND_URL = process.env.BACKEND_URL!;
+
+export class Auth {
+  public static signInWithPassword(email: string, password: string) {
+    
+  }
+}
