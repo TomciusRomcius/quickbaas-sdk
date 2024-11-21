823e2e3dee1023483ae8528b4e49116c413b6a23
Tomas <tomas.ew88@gmail.com>
2024-11-21 13:03:30 +0200

    Add axios + sign up functionality

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
 type AuthConfigType = {
   backendURL: string;
-}
+};
 
 export class Auth {
   static config?: AuthConfigType;
@@ -10,18 +10,41 @@ export class Auth {
   static initialize(config: AuthConfigType) {
     this.config = config;
   }
-  
+
   public static async signInWithPassword(email: string, password: string) {
     if (!Auth.config) {
       // TODO: handle error
       return;
     }
 
-    console.log(this.config?.backendURL);
-    const response = await axios.post(`${Auth.config.backendURL}/user`, {
-      email: "cool email",
-      password: "cool password",
-    });
+    const response = await axios.post(
+      `${Auth.config.backendURL}/auth/sign-in-with-password`,
+      {
+        email: email,
+        password: password,
+      },
+    );
+
+    if (response.status === 200) {
+      const jwt = response.data;
+      localStorage.setItem('auth-cookie', jwt);
+      console.log(jwt);
+    }
+  }
+
+  public static async signUpWithPassword(email: string, password: string) {
+    if (!Auth.config) {
+      // TODO: handle error
+      return;
+    }
+
+    const response = await axios.post(
+      `${Auth.config.backendURL}/auth/sign-up-with-password`,
+      {
+        email: email,
+        password: password,
+      },
+    );
 
     if (response.status === 200) {
       const jwt = response.data;
