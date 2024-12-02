9d7b24630ae95fe5102bc120dce8056fa4a54aee
Tomas <tomas.ew88@gmail.com>
2024-12-02 13:15:18 +0200

    Modify config for building and testing

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const config = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    moduleFileExtensions: ['ts', 'js', 'json'],
+    rootDir: "src",
+};
+exports.default = config;
+//# sourceMappingURL=jest.config.js.map
